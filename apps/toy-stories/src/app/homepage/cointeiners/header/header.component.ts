04949a159ff384383321ebad518f8f1129564d38
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-07 13:25:52 +0100

    ToyStories_Header: Addinh navbar

---
+++ apps/toy-stories/src/app/homepage/cointeiners/header/header.component.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'toy-stories-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
