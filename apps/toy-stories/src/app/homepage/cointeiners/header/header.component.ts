298a9d00458394b77838c9f256554da9cfd5206b
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-13 16:37:08 +0100

    feat(ToyStories): Header
    * ToyStories_Header: Add navbar
    
    * ToyStories__Header: Centered items
    
    * feat(header): reseting default style
    
    * feat(header): reseting default style
    
    * feat(header): change layout and make it more responsive
    
    * feat(header): change layout and make it more responsive
    
    * feat(header): fix typo
    
    * feat(header): fix typo
    
    * WIP: 8ffa304 feat(header): fix typo

---
+++ apps/toy-stories/src/app/homepage/cointeiners/header/header.component.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'ts-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
