3139b77fd19200eb4c25ad55cd5ca9b5432b9948
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-14 17:52:53 +0100

    feat(ToyStories): Fix typo in container folder

--- apps/toy-stories/src/app/app.module.ts
+++ apps/toy-stories/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CategoriesListComponent } from './homepage/cointeiners/categories-list/categories-list.component';
-import { HeaderComponent } from './homepage/cointeiners/header/header.component';
+import { CategoriesListComponent } from './homepage/containers/categories-list/categories-list.component';
+import { HeaderComponent } from './homepage/containers/header/header.component';
 import { HeroComponent } from './homepage/components/hero/hero.component';
-import { FooterComponent } from './homepage/cointeiners/footer/footer.component';
+import { FooterComponent } from './homepage/containers/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
